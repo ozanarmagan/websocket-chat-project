8dcd6796a1b2f7fea919b8bb8ceb003faecf26a8
ozanarmagan <oza-armagan@yandex.com>
2021-03-30 16:37:38 +0300

    second

---
+++ Frontend Windows Forms/WebSocketChatApp/ana.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WebSocketSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebSocketChatApp
+{
+    public partial class ana : Form
+    {
+        Status chat = new Status();
+        WebSocket ws = new WebSocket(statics.websocketurl);
+        public ana()
+        {
+            InitializeComponent();
+        }
+
+        void ws_OnMessage(object sender, WebSocketSharp.MessageEventArgs e)
+        {
+            dynamic jsonparsed = JObject.Parse(e.Data);
+            if (jsonparsed["message_type"] == "online_users")
+            {
+                onlineusers.Invoke(new Action(() => { onlineusers.Items.Clear(); }));
+                foreach (string user in jsonparsed["users"])
+                {
+                    onlineusers.Invoke(new Action(() => { onlineusers.Items.Add(user); }));
+                }
+            }
+            else if(jsonparsed["message_type"] == "new_message")
+            {
+                string newmessage = "["+ jsonparsed.time+"]" + "[" + jsonparsed.user + "]:" + jsonparsed.message;
+                richTextBox1.Invoke(new Action(() => { richTextBox1.Text += newmessage  + Environment.NewLine; }));
+            }
+
+        }
+
+        void ws_OnOpen(object sender,EventArgs e)
+        {
+            string adduser = "{\"type\":\"add_user\",\"content\":\"" + statics.username + "\"}";
+            ws.Send(adduser);
+        }
+
+
+        private void ana_Load(object sender, EventArgs e)
+        {
+            label1.Text = statics.username;
+            ws.OnMessage += ws_OnMessage;
+            ws.OnOpen += ws_OnOpen;
+            ws.Connect();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = textBox1.Text;
+            textBox1.Clear();
+            ws.Send("{\"type\":\"new_message\",\"message\":\"" + message + "\"}");
+        }
+
+        private void ana_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                string message = textBox1.Text;
+                textBox1.Clear();
+                ws.Send("{\"type\":\"new_message\",\"message\":\"" + message + "\"}");
+            }
+        }
+    }
+}
---
+++ Frontend Windows Forms/WebSocketChatApp/statics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebSocketChatApp
+{
+    public static class statics
+    {
+        public static string username;
+        public static string websocketurl = "ws://46.31.79.30:80";
+
+    }
+}
